5ebfec2d72ebf674447b4a9f97dfd49710fd7848
Senzosenkosi <senzoshezi08@gmail.com>
2025-01-11 22:23:44 +0200

    update

--- dash_wrap.py
+++ dash_wrap.py
@@ -6,3 +6,8 @@ st.write("my first app is write here")
 st.balloons()
 
 st.markdown("## Question 1.")
+countdown = st.empty()
+for i in range(10, 0, -1):
+    countdown.markdown(f"## Countdown: {i} seconds")
+    time.sleep(1)
+countdown.markdown("## Time's up!")
