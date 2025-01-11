f73b51d6e15e9438c296e4a784752c12e8cf8482
Senzosenkosi <senzoshezi08@gmail.com>
2025-01-11 22:54:49 +0200

    update

--- dash_wrap.py
+++ dash_wrap.py
@@ -5,12 +5,6 @@ import time
 
 st.write("my first app is write here")
 st.balloons()
-st.set_page_config(
-    page_title="Stock Info",  # Sets the title of the web page
-    page_icon="🏛️",          # Sets the favicon of the web page
-    layout="centered",        # Sets the layout of the page (can be "centered" or "wide")
-    initial_sidebar_state="auto"  # Sets the initial state of the sidebar (can be "auto", "expanded", or "collapsed")
-)
 st.markdown("## Question 1.")
 countdown = st.empty()
 for i in range(30, 0, -1):
