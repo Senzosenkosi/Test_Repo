889a5a5562d8358667bef21014512a984e57b112
Senzosenkosi <senzoshezi08@gmail.com>
2025-01-11 22:43:13 +0200

    update

--- dash_wrap.py
+++ dash_wrap.py
@@ -5,10 +5,13 @@ import time
 
 st.write("my first app is write here")
 st.balloons()
-
+st.set_page_config(
+    page_title="Stock Info",
+    page_icon="🏛️",
+)
 st.markdown("## Question 1.")
 countdown = st.empty()
-for i in range(10, 0, -1):
+for i in range(30, 0, -1):
     countdown.markdown(f"## Countdown: {i} seconds")
     time.sleep(1)
 countdown.markdown("## Time's up!")
