f6e8c83ce53a82ea78ce508047b24d6159584bb8
Senzosenkosi <senzoshezi08@gmail.com>
2025-01-13 12:50:22 +0200

    UPdate

--- dash_wrap.py
+++ dash_wrap.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-
 data = {
     'X': [1,2,3,4,56,66],
     'Y': [2,3,4,5,6,7]
