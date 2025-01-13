c33a490c7e4e3f69284676cdad8fe539199f7937
Senzosenkosi <senzoshezi08@gmail.com>
2025-01-13 12:43:13 +0200

    update

--- dash_wrap.py
+++ dash_wrap.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from  sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
